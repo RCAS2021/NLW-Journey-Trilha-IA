7985ec2d53c7bad3449cf2e0174febea1cee9226
Rafael Cruz <rcas2009@outlook.com>
2024-07-14 11:45:34 -0300

    Preparando a estrutura RAG

--- travelAgent.py
+++ travelAgent.py
@@ -4,6 +4,13 @@ from langchain_community.agent_toolkits.load_tools import load_tools
 from langchain.agents import create_react_agent, AgentExecutor
 from langchain import hub
 
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+import bs4
+
+
 llm = ChatOpenAI(model='gpt-3.5-turbo')
 
 query = """
@@ -22,3 +29,18 @@ def research_agent(query, llm):
     webContext = agent_executor.invoke({'input': query})
     return webContext['output']
 
+def loadData():
+    loader = WebBaseLoader(
+        webpaths=("https://www.dicasdeviagem.com/inglaterra/"),
+        bs_kwargs=dict(parse_only=bs4.SoupStrainer(class_=("postcontentwrap", "pagetitleloading background-imaged loading-dark")))
+    )
+    docs = loader.load()
+
+    # Separando o texto em chunks menores
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+
+    # Salvando no banco de dados vetorial Chroma
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
